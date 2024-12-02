06cd4bea061d979bdb18c35108e56cdcda554d9c
Adam <47026870+RelyingEarth87@users.noreply.github.com>
2024-12-01 23:14:05 -0500

    Update D1P1.py
    Added documentation to the functions

--- D1P1.py
+++ D1P1.py
@@ -1,4 +1,12 @@
 def sorter(data: list[str]) -> tuple[list[int], list[int]]:
+    """Parses and sorts the data into lists and sorts them
+
+    Args:
+        data (list[str]): a list of lines from a document with 2 numbers separated by white space
+
+    Returns:
+        tuple[list[int], list[int]]: 2 lists of sorted integers
+    """
     left = []
     right = []
 
@@ -14,6 +22,15 @@ def sorter(data: list[str]) -> tuple[list[int], list[int]]:
 
 
 def pairer(left: list, right: list) -> list[int]:
+    """Pairs the two lists together and finds the differences between each side
+
+    Args:
+        left (list): a sorted list of integers
+        right (list): a sorte list of integers
+
+    Returns:
+        list[int]: a list of the differences between the data in each list
+    """
     differences = []
     for i in range(len(left)):
         differences.append(abs(left[i] - right[i]))
@@ -21,6 +38,8 @@ def pairer(left: list, right: list) -> list[int]:
     return differences
 
 def main():
+    """Imports files, calls functions, and prints the sum of the differences to present the data
+    """
     with open('files/D1.txt', 'r') as f:
         lines = f.readlines()
     
