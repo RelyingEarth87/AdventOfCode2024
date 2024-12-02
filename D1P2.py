bf794f5ff827732d719163c9b08d0aaa2eb0d747
Adam <47026870+RelyingEarth87@users.noreply.github.com>
2024-12-01 23:10:19 -0500

    Update D1P2.py
    Added documentation for functions

--- D1P2.py
+++ D1P2.py
@@ -1,6 +1,14 @@
 import numpy as np
 
-def sorter(data: list[str]):
+def sorter(data: list[str]) -> np.array:
+    """Parses and sorts the data into lists and a sorted numpy array
+
+    Args:
+        data (list[str]): a list of lines from a document with 2 numbers separated by white space
+
+    Returns:
+        np.array: A 2D numpy array with 2 columns of integers sorted from low to high values
+    """
     left = []
     right = []
 
@@ -13,7 +21,15 @@ def sorter(data: list[str]):
     
     return np.sort(arr)
 
-def pairer(arr: list[int]):
+def pairer(arr: np.array) -> tuple[list[int], list[int]]:
+    """Pairs the data from the right an left columns of data and uses a filtered numpy array to get the counts of each data point from the left column in the right column
+
+    Args:
+        arr (np.array[int]): A 2D numpy array with 2 columns of integers sorted from low to high values
+
+    Returns:
+        tuple[list[int], list[int]]: a list of the left column of data and a list of the amount of times each integer appears in the right column
+    """
     left = arr[0]
     right = arr[1]
     counts = []
@@ -28,7 +44,16 @@ def pairer(arr: list[int]):
     
     return left, counts
 
-def multiplier(left, counts):
+def multiplier(left: list[int], counts: list[int]) -> int:
+    """Takes a list of data and the amount of times each piece of data appears and multiplies them together to get a similarity score
+
+    Args:
+        left (list[int]): a list of data
+        counts (list[int]): the amount of times each number appears in a set of data
+
+    Returns:
+        int: a similarity score  calculated using the sum of each number in the list, times the amount it appears
+    """
     similarity = 0
     for i in range(len(left)):
         addend = left[i] * counts[i]
@@ -37,6 +62,8 @@ def multiplier(left, counts):
     return similarity
 
 def main():
+    """A main funcion to handle calling other functions and presenting data
+    """
     with open('files/D1.txt', 'r') as f:
         lines = f.readlines()
     
